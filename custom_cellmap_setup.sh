5faf1e6a8ec12a3872edddcd7145f1201eeadc36
niksa-poleksic <niksa-poleksic@uiowa.edu>
2023-08-15 12:29:46 -0500

    Bash progress

--- custom_cellmap_setup.sh
+++ custom_cellmap_setup.sh
@@ -1,3 +1,82 @@
-conda env create -f ClearMapHPC.yml -y
-conda activate ClearMapHPC
-python -m ipykernel install --user --name ClearMapHPC --display-name “ClearMapHPC”
+#!/bin/bash
+
+echo -e "\n------------------------------------------------------------------------------------"
+echo "--------------------------------------CELLMAP---------------------------------------"
+echo "------------------------------------------------------------------------------------"
+
+echo -e "\nThis script will setup the necessary environments for manually running CellMap."
+
+echo -e "\nTo run a basic CellMap pipeline, exit this script and run ./run_cellmap.sh\n"
+
+echo "The script will perform the following processes:"
+echo -e "\t - Create and activate a CellMap-specific conda environment"
+echo -e "\t - Install a python kernel that allows the user to manually run CellMap"
+
+read -r -p "Would you like to proceed? ([y]/n): " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    echo -e "\nInitializing..."
+else
+    echo -e "\nExiting script...\n"
+    exit 0
+fi
+
+eval "$(conda shell.bash hook)" # for activating conda environments
+echo -e "\nChecking for environment...\n"
+
+conda env list | grep -q "ClearMapHPC"
+if [ $? -eq 0 ]; then
+    echo "The conda environment \"ClearMapHPC\" already exists."
+    echo "Activating ClearMapHPC..."
+    conda activate ClearMapHPC
+    echo "ClearMapHPC successfully activated!"
+else
+    echo "The conda environment \"ClearMapHPC\" is not installed."
+    echo -e "\nIn order to function properly, CellMap requires a specific environment."
+    read -r -p "Would you like to install it? ([y]/n): " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo "Creating conda environment from ClearMapHPC.yml..."
+        echo "(This may take a few minutes)"
+        conda env create -f ClearMapHPC.yml -y
+        echo "Environment successfully installed!"
+        echo "Activating ClearMapHPC..."
+        conda activate ClearMapHPC
+        echo "ClearMapHPC successfully activated!"
+    else
+        echo -e "\nExiting script...\n"
+        exit 0
+    fi
+fi
+
+echo -e "\nChecking for kernel...\n"
+
+jupyter kernelspec list | grep -q "clearmaphpc"
+if [ $? -eq 0 ]; then
+    echo "The python kernel \"ClearMapHPC\" already exists."
+else
+    echo "The python kernel \"ClearMapHPC\" is not installed."
+    echo -e "\nTo run CellMap from a console editor, the \"ClearMapHPC\" kernel is required"
+    read -r -p "Would you like to install it? ([y]/n): " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo "Installing ipykernel..."
+        echo "(This may take a few minutes)"
+        conda install -y ipykernel
+        echo "ipykernel successfully installed!"
+        echo "Creating kernel..."
+        python -m ipykernel install --user --name ClearMapHPC --display-name “ClearMapHPC”
+        echo "\"ClearMapHPC\" kernel successfully created!"
+    else
+        echo -e "\nExiting script...\n"
+        exit 0
+    fi
+fi
+
+echo -e "\nTo run CellMap from a console editor on JupyterHub:"
+echo -e "\t - Open CellMap.py in the Scripts directory"
+echo -e "\t - Right click inside the file and select \"Create Console for Editor\""
+echo -e "\t - In the \"Select Kernel\" dropdown menu, select \"ClearMapHPC\""
+echo -e "\t\t - This should open a console tab where you can view outputs"
+echo -e "\t - Highlight desired code within CellMap.py"
+echo -e "\t - Press [Shift]+[Enter]"
--- run_cellmap.sh
+++ run_cellmap.sh
@@ -3,9 +3,14 @@
 echo -e "\n------------------------------------------------------------------------------------"
 echo "--------------------------------------CELLMAP---------------------------------------"
 echo "------------------------------------------------------------------------------------"
+
 echo -e "\nThis script is intended for a basic execution of the CellMap pipeline."
-echo -e "\nThe script will perform the following processes:"
-echo -e "\t - Create and activate a conda environment necessary for executing CellMap"
+
+
+echo -e "\nFor a complete customizable experience, exit this script and run ./custom_cellmap_setup.sh\n"
+
+echo "The script will perform the following processes:"
+echo -e "\t - Create and activate a CellMap-specific conda environment"
 echo -e "\t - Import required pipeline modules"
 echo -e "\t - Allow the user to configure the pipeline with various parameters"
 echo -e "\t - Execute the full CellMap pipeline which will:"
@@ -13,11 +18,47 @@ echo -e "\t\t - Resample the provided data"
 echo -e "\t\t - Align the data to the reference atlas"
 echo -e "\t\t - Detect, filter, align and annotate cells"
 echo -e "\t\t - Perform voxelization and cell density analysis"
-echo -e "\t\t - Export all results to the provided experimental directory"
+echo -e "\t\t - Export all results to the provided experimental directory\n"
 
-echo -e "\nFor a more customizable experience, exit this script and run /.custom_cellmap_setup.sh"
+echo "VERIFY BEFORE RUNNING THE PIPELINE:"
+echo -e "\t - Experimental directory contains only two folders (for raw and autof data)"
+echo -e "\t - Experimental data is cropped to remove excess empty space\n"
 
-echo -e "Would you like to begin executing the CellMap pipeline? ([y]/n)"
+read -r -p "Would you like to proceed with the CellMap pipeline? ([y]/n): " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    echo -e "\nInitializing pipeline..."
+else
+    echo -e "\nExiting script...\n"
+    exit 0
+fi
 
-conda env create -f ClearMapHPC.yml -y
-conda activate ClearMapHPC
+eval "$(conda shell.bash hook)" # for activating conda environments
+echo -e "\nChecking for environment...\n"
+
+conda env list | grep -q "ClearMapHPC"
+if [ $? -eq 0 ]; then
+    echo "The conda environment \"ClearMapHPC\" already exists."
+    echo "Activating ClearMapHPC..."
+    conda activate ClearMapHPC
+    echo "ClearMapHPC successfully activated!"
+else
+    echo "The conda environment \"ClearMapHPC\" is not installed."
+    echo -e "\nThe CellMap pipeline can only proceed within the proper environment"
+    read -r -p "Would you like to install it? ([y]/n): " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo -e "Creating conda environment from ClearMapHPC.yml..."
+        echo "(This may take a few minutes)"
+        conda env create -f ClearMapHPC.yml -y
+        echo -e "Environment successfully installed!"
+        echo "Activating ClearMapHPC..."
+        conda activate ClearMapHPC
+        echo "ClearMapHPC successfully activated!"
+    else
+        echo -e "\nExiting script...\n"
+        exit 0
+    fi
+fi
+
+echo -e "\nSUCCESS\n"
