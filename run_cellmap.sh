52f5ac9c15146f5b91f7923cba06009995634bb6
niksa-poleksic <niksa-poleksic@uiowa.edu>
2024-03-13 11:43:37 -0500

    Refactoring

--- run_cellmap.sh
+++ run_cellmap.sh
@@ -4,17 +4,13 @@ echo -e "\n---------------------------------------------------------------------
 echo "--------------------------------------CELLMAP---------------------------------------"
 echo "------------------------------------------------------------------------------------"
 
-echo -e "\nThis script is intended for a basic execution of the CellMap pipeline."
-
-echo -e "\nFor a complete customizable experience, exit this script and run ./custom_cellmap_setup.sh\n"
+echo -e "\nThis script is intended for a basic execution of the CellMap pipeline.\n"
 
 echo "The script will perform the following processes:"
 echo -e "\t - Create and activate a CellMap-specific conda environment"
 echo -e "\t - Import required pipeline modules"
-echo -e "\t - Allow the user to configure the pipeline with various parameters"
 echo -e "\t - Execute the full CellMap pipeline which will:"
-echo -e "\t\t - Resample the provided data"
-echo -e "\t\t - Align the data channels with the reference atlas"
+echo -e "\t\t - Align the data channels with a reference brain atlas"
 echo -e "\t\t - Detect, filter, align and annotate cells"
 echo -e "\t\t - Perform voxelization and cell density analysis"
 echo -e "\t\t - Export all results to the provided experimental directory\n"
